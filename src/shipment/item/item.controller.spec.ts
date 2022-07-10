b007e2a5949cedcc18cd3d31a487a7187c5f74c7
Yimesgen Morka <dochomorka@gmail.com>
2022-07-11 02:48:34 +0300

    added test case for findAll in item controller

--- src/shipment/item/item.controller.spec.ts
+++ src/shipment/item/item.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { async } from 'rxjs';
 import { Item } from './entities/item.entity';
 import { ItemController } from './item.controller';
 import { ItemService } from './item.service';
@@ -18,9 +19,21 @@ describe('ItemController', () => {
     }).compile();
 
     controller = module.get<ItemController>(ItemController);
+    service = module.get<ItemService>(ItemService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return a list of item', async () => {
+      const result = [];
+      const spy = jest.spyOn(service, 'findAll').mockResolvedValue(result);
+      const id = '1';
+
+      expect(await controller.findAll(id)).toBe(result);
+      expect(spy).toBeCalledWith(id);
+    });
   });
 });
