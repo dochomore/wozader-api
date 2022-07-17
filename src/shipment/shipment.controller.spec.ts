fdd10a98c87fc806c6863379d0acca7038d5fab6
Yimesgen Morka <dochomorka@gmail.com>
2022-07-18 02:12:32 +0300

    setting up dependencies for shipment controller test

--- src/shipment/shipment.controller.spec.ts
+++ src/shipment/shipment.controller.spec.ts
@@ -1,17 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Shipment } from './entities/shipment.entity';
 import { ShipmentController } from './shipment.controller';
 import { ShipmentService } from './shipment.service';
 
 describe('ShipmentController', () => {
   let controller: ShipmentController;
+  let service: ShipmentService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ShipmentController],
-      providers: [ShipmentService],
+      providers: [
+        ShipmentService,
+        { provide: getRepositoryToken(Shipment), useValue: {} },
+      ],
     }).compile();
 
     controller = module.get<ShipmentController>(ShipmentController);
+    service = module.get<ShipmentService>(getRepositoryToken(Shipment));
   });
 
   it('should be defined', () => {
