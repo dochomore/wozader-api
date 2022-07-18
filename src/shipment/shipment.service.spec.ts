88c1227e1a1768e10dc946fe9c9a141685535187
Yimesgen Morka <dochomorka@gmail.com>
2022-07-18 03:32:54 +0300

    setting up test dependencies

--- src/shipment/shipment.service.spec.ts
+++ src/shipment/shipment.service.spec.ts
@@ -1,15 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Shipment } from './entities/shipment.entity';
 import { ShipmentService } from './shipment.service';
 
+const mockRepository = () => ({});
 describe('ShipmentService', () => {
   let service: ShipmentService;
+  let repository: Repository<Shipment>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ShipmentService],
+      providers: [
+        ShipmentService,
+        { provide: getRepositoryToken(Shipment), useFactory: mockRepository },
+      ],
     }).compile();
 
     service = module.get<ShipmentService>(ShipmentService);
+    repository = module.get(getRepositoryToken(Shipment));
   });
 
   it('should be defined', () => {
