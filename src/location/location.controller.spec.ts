ef0cae9d272d3398e10962e9b1a6e50329ba02d5
Yimesgen Morka <dochomorka@gmail.com>
2022-07-19 08:05:36 +0300

    setup testing dependencies for Location controllers

--- src/location/location.controller.spec.ts
+++ src/location/location.controller.spec.ts
@@ -1,17 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Location } from './entities/location.entity';
 import { LocationController } from './location.controller';
 import { LocationService } from './location.service';
 
 describe('LocationController', () => {
   let controller: LocationController;
+  let service: LocationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LocationController],
-      providers: [LocationService],
+      providers: [
+        LocationService,
+        { provide: getRepositoryToken(Location), useValue: {} },
+      ],
     }).compile();
 
     controller = module.get<LocationController>(LocationController);
+    service = module.get<LocationService>(LocationService);
   });
 
   it('should be defined', () => {
