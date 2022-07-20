4815f3a2367dfd69acf32aa29b1af20bcf1f61f8
Yimesgen Morka <dochomorka@gmail.com>
2022-07-20 08:07:56 +0300

    setting up dependences for location test file

--- src/location/location.service.spec.ts
+++ src/location/location.service.spec.ts
@@ -1,15 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Location } from './entities/location.entity';
 import { LocationService } from './location.service';
+
+const mockRepository = () => ({});
 
 describe('LocationService', () => {
   let service: LocationService;
+  let repository: Repository<Location>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LocationService],
+      providers: [
+        LocationService,
+        { provide: getRepositoryToken(Location), useFactory: mockRepository },
+      ],
     }).compile();
 
     service = module.get<LocationService>(LocationService);
+    repository = module.get(getRepositoryToken(Location));
   });
 
   it('should be defined', () => {
