ed08e9c8c995e3e7e08dbb54f8effc41e83fa1a5
Yimesgen Morka <dochomorka@gmail.com>
2022-07-23 04:23:06 +0300

    added unique constraint to username field

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, MinLength } from 'class-validator';
+import { IsDefined, MinLength } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
@@ -6,12 +6,12 @@ export class User {
   @PrimaryGeneratedColumn('uuid')
   userId: string;
 
-  @Column()
-  @IsNotEmpty()
+  @Column({ unique: true })
+  @IsDefined()
   username: string;
 
-  @Column()
-  @IsNotEmpty()
+  @Column({ unique: true })
+  @IsDefined()
   @MinLength(8)
   password: string;
 }
