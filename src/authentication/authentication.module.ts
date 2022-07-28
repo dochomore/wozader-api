bb4df4394a7a71508acee135f6551099dbf5ae28
Yimesgen Morka <dochomorka@gmail.com>
2022-07-28 09:02:00 +0300

    added user validation with bcrypt

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [UserModule],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService]
+  providers: [AuthenticationService],
 })
 export class AuthenticationModule {}
--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(username: string, password: string) {
+    const user = await this.userService.findOneByUsername(username);
+    if (user instanceof User) {
+      if (await bcrypt.compare(password, user.password)) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { password, ...others } = user;
+        return others;
+      }
+    }
+    return null;
+  }
+}
