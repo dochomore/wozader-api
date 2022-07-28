2763e4c195634ef2791f1f685743b4142e5b588a
Yimesgen Morka <dochomorka@gmail.com>
2022-07-28 10:05:46 +0300

    added packages for passport

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,11 +1,23 @@
-import { IsDefined, IsString } from 'class-validator';
+import { IsDefined } from 'class-validator';
 
 export class CreateUserDto {
-  @IsString()
   @IsDefined()
   username: string;
 
   @IsDefined()
-  @IsString()
+  firstName: string;
+
+  @IsDefined()
+  fatherName: string;
+
+  @IsDefined()
+  createdAt: string;
+
+  @IsDefined()
+  updatedAt: string;
+
+  @IsDefined()
   password: string;
+
+  imageUrl?: string;
 }
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,4 +1,3 @@
-import { IsDefined } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 /**
@@ -11,27 +10,21 @@ export class User {
   userId: string;
 
   @Column({ unique: true })
-  @IsDefined()
   username: string;
 
   @Column()
-  @IsDefined()
   firstName: string;
 
   @Column()
-  @IsDefined()
   fatherName: string;
 
   @Column()
-  @IsDefined()
   createdAt: string;
 
   @Column()
-  @IsDefined()
   updatedAt: string;
 
   @Column()
-  @IsDefined()
   password: string;
 
   @Column({ nullable: true })
